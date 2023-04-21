724c7d03071e612ebd6db92f7a3b7baf0f1ebb62
Noah Caple <caple.noah@yahoo.com>
2023-04-20 23:43:43 -0400

    dtuff

--- auth.js
+++ auth.js
@@ -8,12 +8,21 @@ const TOKEN = "https://accounts.spotify.com/api/token";
 
 const login_btn = document.getElementById("login_btn");
 
+// Function order =>
+// onPageLoad()
+// handleRedirect()
+// getCode()
+// fetchAccessToken()
+// callAuthorizationApi()
+
 //Runs on body load, handleRedirect if any headers are present
 function onPageLoad() {
   console.log("onPageLoad fired");
   if (window.location.search.length > 0) {
     handleRedirect();
   }
+  const me = getMe()
+  console.log(me)
 }
 
 //Fetched code with getCode, uses code to fetchAccessToken
@@ -39,6 +48,7 @@ function requestAuthorization() {
   window.location.href = url; //set URL to built URL
 }
 
+//Returns code scraped from end of redirect URL after requestAuthorization
 function getCode() {
   console.log("getCode fired");
   let code = null;
@@ -50,6 +60,7 @@ function getCode() {
   return code;
 }
 
+//Build request body, send auth request using callAuthorizationApi, send result to handleAuthResponse
 function fetchAccessToken(code) {
   console.log("fetchAccessToken fired");
   let body = "grant_type=authorization_code";
@@ -58,9 +69,12 @@ function fetchAccessToken(code) {
   body += "&client_id=" + client_id;
   body += "&client_secret=" + client_secret;
   //console.log(body)
-  handleAuthResponse(callAuthorizationApi(body));
+  callAuthorizationApi(body).then((response) => {
+    handleAuthResponse(response);
+  });
 }
 
+//Send POST to /token end point, return JSON object containing access_token
 async function callAuthorizationApi(body) {
   console.log("callAuthorizationApi ran");
   const response = await fetch(TOKEN, {
@@ -71,21 +85,39 @@ async function callAuthorizationApi(body) {
       Authorization: "Basic " + window.btoa(client_id + ":" + client_secret),
     },
     body: body,
-  }).then((response) => response.json())
-
-  const data = () => {
-    response.then((a) =>{
-        console.log(a)
-    })
-  }
-
-  data();
-
+  });
+  return response.json();
 }
 
+//save all gathered tokens/data to internalStorage
 function handleAuthResponse(data) {
   console.log("handleAuthorizationResponse ran");
-  console.log(data)
+  localStorage.setItem("access_token", data.access_token);
+  localStorage.setItem("refresh_token", data.refresh_token);
+}
+
+function sendHttpRequest(method, url, headers, body) {
+  console.log("sendHttpRequest to", url);
+  return fetch(url, {
+    method: method,
+    headers: headers,
+    body: body,
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
+
+function getMe() {
+  method = 'Get'
+  url = 'https://api.spotify.com/v1/me'
+  headers = { "Authorization": "Bearer " + localStorage.getItem("access_token")}
+  body = null;
+  const data = sendHttpRequest(method, url, headers, body)
+    .then(response => response)
+    .then(data => { return data })
+    .catch(error => console.error(error));
+
+    console.log(data)
 }
 
 // Call requestAuthorization on login_btn click
